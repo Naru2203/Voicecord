2c195e8a80da664b71e4f85a8db46457bef855b7
Naruv <101044276+Naru7@users.noreply.github.com>
2022-03-08 09:40:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import os
 import discord
+import time
 from keep_alive import keep_alive
 
+print("""\
+██╗░░░██╗░█████╗░██╗░█████╗░███████╗░█████╗░░█████╗░██████╗░██████╗░
+██║░░░██║██╔══██╗██║██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+╚██╗░██╔╝██║░░██║██║██║░░╚═╝█████╗░░██║░░╚═╝██║░░██║██████╔╝██║░░██║
+░╚████╔╝░██║░░██║██║██║░░██╗██╔══╝░░██║░░██╗██║░░██║██╔══██╗██║░░██║
+░░╚██╔╝░░╚█████╔╝██║╚█████╔╝███████╗╚█████╔╝╚█████╔╝██║░░██║██████╔╝
+░░░╚═╝░░░░╚════╝░╚═╝░╚════╝░╚══════╝░╚════╝░░╚════╝░╚═╝░░╚═╝╚═════╝░
+**Version: 1.0.0**""")
+time.sleep(0.5)
 client = discord.Client()
 
 @client.event
 async def on_ready():
   
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="The Forest Grand"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Voicecord"))
   
     voice_channel = client.get_channel(Your id channel)
     await voice_channel.connect()
   
     print('Logged in as {0.user}'.format(client))
-    print('Successful connect to voice channel.')
-  
+    print('Connected to voice channel {}'.format(voice_channel))
+    
 keep_alive()
 client.run(os.getenv("TOKEN"), bot = False)
 
