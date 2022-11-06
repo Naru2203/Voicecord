b7e1362f842da144d821bd8d37c3d08417a6bba2
Naruv <101044276+Naru2203@users.noreply.github.com>
2022-11-06 15:17:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,21 @@ print("""\
 ░░░╚═╝░░░░╚════╝░╚═╝░╚════╝░╚══════╝░╚════╝░░╚════╝░╚═╝░░╚═╝╚═════╝░
 **Version: 1.0.0**""")
 time.sleep(0.5)
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
+Token = input("Please Enter Your Token: ")
+Id = int(input("Please Enter Your Channel ID: "))
+
 
 @client.event
 async def on_ready():
-  
-    voice_channel = client.get_channel(add here) #<= add specific id voice channel to connect 
+    voice_channel = client.get_channel(Id) 
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Lofi"))
     await voice_channel.connect()
   
     print('Logged in as {0.user}'.format(client))
-    print('Connected to voice channel {}'.format(voice_channel))
+    print('Connected to voice channel: {}'.format(voice_channel))
     
 keep_alive()
-client.run(os.getenv("TOKEN"), bot = False)
+client.run(Token, bot = False)
 
 
